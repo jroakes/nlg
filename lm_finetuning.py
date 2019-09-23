f823be389dedc253c3a07798efaa8c1fa3a5ddb9
JR Oakes <jroakes@gmail.com>
2019-09-22 21:17:19 -0400

    Debug

--- lm_finetuning.py
+++ lm_finetuning.py
@@ -186,6 +186,7 @@ def train(args, train_dataset, model, tokenizer):
     for _ in train_iterator:
         epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0], position=0, leave=True)
         for step, batch in enumerate(epoch_iterator):
+            print(batch)
             inputs, labels = mask_tokens(batch, tokenizer, args) if args.mlm else (batch, batch)
             if not args.mlm:
                 # Mask labels
